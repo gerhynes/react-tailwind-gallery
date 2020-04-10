0fba58bb365b59e5e7f4c8d98076329250dbcbca
Gerard Hynes <gkhynes@gmail.com>
2020-04-10 11:22:47 +0100

    Add loading message

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -25,9 +25,9 @@ const ImageCard = ({ image }) => {
         </ul>
       </div>
       <div className="px-6 py-4">
-        {tags.map((tag) => (
+        {tags.map((tag, index) => (
           <span
-            key={tag}
+            key={index}
             className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
           >
             #{tag}
