8120197fd6a8569eeeabf658d69d5a43fd532d49
Gerard Hynes <gkhynes@gmail.com>
2020-05-24 15:18:43 +0100

    Make gallery responsive

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {images.map((image) => (
             <ImageCard key={image.id} image={image} />
           ))}
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const ImageCard = ({ image }) => {
   const tags = image.tags.split(",");
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
       <img src={image.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
